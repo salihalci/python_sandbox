a63df120467b63c29799a92c7cfaff40c1abaa28
salihalci <salihalci@gmail.com>
2023-03-02 21:20:12 +0300

    added new operation and loop

--- dictonarysample.py
+++ dictonarysample.py
@@ -1,11 +1,22 @@
 def sumfunc(a,b):
+    """math sum operation"""
     return a+b
 
+def multiply(a,b):
+    """math multiply operation"""
+    return a*b
 
+
+
+#assign a function to a dictionary item
 operations = {
-    "*":sumfunc
+    "+":sumfunc,
+    "*":multiply
 }
 
 totaloperation=operations["*"]
 
 print(totaloperation(2,5))
+
+for _ in operations:
+    print(_)
