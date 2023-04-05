8a3486e2a4af5e9e6c8c8ac89473b11123637fdb
salihalci <salihalci@gmail.com>
2023-04-05 18:55:47 +0300

    classes added

--- classex/MyClasses.py
+++ classex/MyClasses.py
@@ -1,3 +1,12 @@
 class Car:
-    pass
+
+    def __init__(self, user_id):
+        self.user_id=user_id
+        print(self.user_id)
+
+car2 = Car("abc")
+print(car2.user_id)
+
+car1 = Car()
+print("MyCar!")
 
