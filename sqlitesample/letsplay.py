f82d344e5af8c5780fb7aab6f9fb242690a805d6
salihalci <salihalci@gmail.com>
2023-04-05 21:39:40 +0300

    many customers insert operation

--- sqlitesample/letsplay.py
+++ sqlitesample/letsplay.py
@@ -13,3 +13,22 @@ many_customers=[('Ali','Kırmızı','alikirmizi@gmail.com')]
 
 conn.commit()
 conn.close()
+import sqlite3
+
+#conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect('customer.db')
+#if db does not exist it will create it.
+
+c=conn.cursor()
+
+many_customers = [
+    ('Orhan','Polat','orhanpolat@gmail.com'),
+    ('Gokmen','Doğutürk','gokmendoguturk@gmail.com')
+]
+
+c.executemany("INSERT INTO CUSTOMERS VALUES(?,?,?)",many_customers)
+print("many customers inserted successfully!")
+
+#Commit our command
+conn.commit()
+conn.close()
