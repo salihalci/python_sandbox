c96f66d902e4a12a2f0c27ec41b9a482b71b0cf5
salci <salih.alci@gmail.com>
2024-03-10 19:55:32 +0300

    form created but not completed.

--- memory_card_app/app.py
+++ memory_card_app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/editquery",methods=["GET","POST"])
+def edıtquery():
+    question = request.form.get('question')
+    print(f"{question} and")
+    
+    return render_template("editquery.html")
 
 if __name__=="__main__":
     app.run(debug=True)
