7011157e8982e49ab4e2d290e80f50c472595e2a
salci <salih.alci@gmail.com>
2024-06-23 22:41:12 +0300

    updated.

--- django/personal_portfolio_project/blog/urls.py
+++ django/personal_portfolio_project/blog/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 from . import views
 from django.urls import include
+
+app_name = 'blog'
 urlpatterns = [
    
     path('',views.all_blogs,name='all_blogs'),
+    path('<int:blog_id>/',views.detail,name='detail'),
   
 ]
