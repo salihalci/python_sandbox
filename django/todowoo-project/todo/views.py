baa9a9bdd9b5ab0a9c7db968df496611a6635717
salci <salih.alci@gmail.com>
2024-06-24 11:31:05 +0300

    signup works but not completed.

--- django/todowoo-project/todo/views.py
+++ django/todowoo-project/todo/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 # Create your views here.
 def signupuser(request):
-    return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+    else:
+        #Create a new user submit button clicked
+        if request.POST['password1'] == request.POST['password2']:
+          user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+          user.save()
+          return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+        else:
+            #Password did not match
+            print("password did not match")
+            pass
