cc7aaa829ae7010508f75ba0cc306bfcba2b4c1b
salci <salih.alci@gmail.com>
2024-06-24 17:35:25 +0300

    logout function added.

--- django/todowoo-project/todowoo/urls.py
+++ django/todowoo-project/todowoo/urls.py
@@ -21,7 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Auth 
     path('signup/',views.signupuser,name='signupuser'),
+    path('logout/',views.logoutuser,name='logoutuser'),
     #TODOs
     path('current/',views.currenttodos,name='currenttodos'),
+    path('',views.home,name='home'),
 
 ]
