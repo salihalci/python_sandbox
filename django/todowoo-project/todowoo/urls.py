5b3b14abc31e923655f3b1e914615b01420cb35e
salci <salih.alci@gmail.com>
2024-06-25 21:32:08 +0300

    added login but not finished.

--- django/todowoo-project/todowoo/urls.py
+++ django/todowoo-project/todowoo/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     #Auth 
     path('signup/',views.signupuser,name='signupuser'),
     path('logout/',views.logoutuser,name='logoutuser'),
+    path('login/',views.loginuser,name='loginuser'),
     #TODOs
     path('current/',views.currenttodos,name='currenttodos'),
     path('',views.home,name='home'),
