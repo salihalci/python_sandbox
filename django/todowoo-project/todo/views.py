1d4a9ac0d97fcdb1740cbc246a233a9f7e5c407d
salci <salih.alci@gmail.com>
2024-06-25 21:53:50 +0300

    fixed wrong login problem.

--- django/todowoo-project/todo/views.py
+++ django/todowoo-project/todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
 
 
 def home(request):
@@ -37,24 +37,12 @@ def logoutuser(request):
     if request.method=='POST':
         logout(request)
         return redirect('home')
+
 def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-        # Create a new user submit button clicked
-        if request.POST['password1'] == request.POST['password2']:
+        user = authenticate(request,username=request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(),'error':'Username and password did not match!'})
 
-            try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-
-                user.save()
-                login(request, user)
-                return redirect('currenttodos')
-            except IntegrityError:
-                return render(request, 'todo/signupuser.html',
-                              {'form': UserCreationForm(), 'error': 'Username has already been taken!'})
-
-        else:
-            # Password did not match
-            return render(request, 'todo/signupuser.html',
-                          {'form': UserCreationForm(), 'error': 'Passwords did not match!'})
