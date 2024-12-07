eba00bbc7c63c2eb697b5f56f7b078053dada76e
salci <salihalci@gmail.com>
2024-12-07 23:17:54 +0300

    create recortd form added to /tasklist/

---
+++ django/todo_list/base/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+
+    title=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add a New Task'}))
+    description=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Description'}))
+    
+    class Meta:
+        model = Task
+        fields ='__all__'
--- django/todo_list/base/views.py
+++ django/todo_list/base/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
 from .models import Task
 
+from .forms import TaskForm
 
 def index(request):
     return HttpResponse("HelloWorld!")
 
 
 def taskList(request):
-     return render(request,'base/tasklist.html')
-     #return HttpResponse("tasklist!")
+
+    form =TaskForm()
+
+    tasks = Task.objects.all()
+
+
+    if request.method =='POST':
+        form= TaskForm(request.POST)
+
+        if form.is_valid():
+            try:
+                form.save()
+                print("Form saved")
+            except: 
+                print("Hata")
+            
+            return redirect('/tasklist')
+
+        
+
+    context = {'tasks':tasks,'TaskForm':form}
+
+    return render(request,'base/tasklist.html',context)
+    
