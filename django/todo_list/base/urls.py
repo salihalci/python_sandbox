d95b1ff40034788fe515e539cbfd4bc19910c031
salci <salihalci@gmail.com>
2024-12-08 19:05:01 +0300

    delete function added

--- django/todo_list/base/urls.py
+++ django/todo_list/base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('tasklist/', views.taskList, name="tasklist"),
     path('updatetask/<str:pk>', views.updateTask, name="updatetask"),
+    path('deletetask/<str:pk>', views.deleteTask, name="deletetask"),
     
 
 ]
--- django/todo_list/base/views.py
+++ django/todo_list/base/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Task
 
 from .forms import TaskForm
+
 
 def index(request):
     return HttpResponse("HelloWorld!")
@@ -11,46 +12,56 @@ def index(request):
 
 def taskList(request):
 
-    form =TaskForm()
+    form = TaskForm()
 
     tasks = Task.objects.all()
 
-
-    if request.method =='POST':
-        form= TaskForm(request.POST)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
 
         if form.is_valid():
             try:
                 form.save()
                 print("Form saved")
-            except: 
+            except:
                 print("Hata")
-            
+
             return redirect('/tasklist')
 
-        
+    context = {'tasks': tasks, 'TaskForm': form}
 
-    context = {'tasks':tasks,'TaskForm':form}
+    return render(request, 'base/tasklist.html', context)
 
-    return render(request,'base/tasklist.html',context)
-    
-def updateTask(request,pk):
-    
+
+def updateTask(request, pk):
+
     task = Task.objects.get(id=pk)
 
     form = TaskForm(instance=task)
 
+    print("Update form loaded.")
 
+    if request.method == 'POST':
 
-    if request.method =='POST':
-        
-        form= TaskForm(request.POST,instance=task)
+        form = TaskForm(request.POST, instance=task)
 
         if form.is_valid():
-            
+
             form.save()
-                
+            print("Update executed.")
             return redirect('/tasklist')
-    
-    context = {'TaskForm':form}
-    return render(request,'base/updatetask.html',context)
+
+    context = {'TaskForm': form}
+    return render(request, 'base/updatetask.html', context)
+
+
+def deleteTask(request, pk):
+    print("Delete form opened.")
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        print("Delete executed.")
+        task.delete()
+        return redirect('tasklist')
+
+    context = {'task': task}
+    return render(request, 'base/deletetask.html', context)
