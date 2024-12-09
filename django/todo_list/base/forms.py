d7530e9db4196a5399b6fae312e8a5565c9bb54d
salci <salihalci@gmail.com>
2024-12-09 14:10:55 +0300

    css applied but need to rework

--- django/todo_list/base/forms.py
+++ django/todo_list/base/forms.py
@@ -3,11 +3,14 @@ from django.forms import ModelForm
 
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
 
-    title=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add a New Task'}))
-    description=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Description'}))
-    
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Add a New Task'}))
+    description = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Description'}))
+
     class Meta:
         model = Task
-        fields ='__all__'
+        fields = '__all__'
